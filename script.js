aed1cde21e7ee08c129ac3475dc49b0de80071db
noguchi <02125011a@gmail.com>
2021-09-28 01:16:31 +0900

    update: スワイプした後にアラートが出る

--- script.js
+++ script.js
@@ -1,18 +1,14 @@
-/*
- * グローバル変数
- */
+// グローバル変数
 let no; // 数値格納用
 let number; // 数値表示部分のDOM取得用
 let s;
+let startY; // タッチ開始 y座標
+let moveY; // スワイプ中の y座標
+let dist = 0; // スワイプを感知する最低距離（ピクセル単位）
 
-/*
- * スワイプイベント設定
- */
+//  スワイプイベント設定
 function setSwipe(elem) {
   let t = document.querySelector(elem);
-  let startY; // タッチ開始 y座標
-  let moveY; // スワイプ中の y座標
-  let dist = 0; // スワイプを感知する最低距離（ピクセル単位）
 
   // タッチ開始時： xy座標を取得
   t.addEventListener("touchstart", function (e) {
@@ -30,28 +26,30 @@ function setSwipe(elem) {
     if (startY > moveY && startY > moveY + dist) {
       s = startY - moveY;
       setNumber();
+
+      if (0 < s && s < 100) {
+        alert("しょう");
+      } else if (100 <= s && s < 200) {
+        alert("ちゅう");
+      } else if (200 <= s && s < 300) {
+        alert("じょう");
+      } else if (300 <= s && s < 400) {
+        alert("さい");
+      } else {
+        alert("ぷんぷん");
+      }
     }
   });
 }
 
-/*
- * 数値を画面に表示する
- */
+// 数値を画面に表示する
 function setNumber() {
   number.innerHTML = s;
 }
 
-/*
- * 起動時の処理
- */
+//  起動時の処理
 window.addEventListener("load", function () {
-  // 数値表示部分のDOM取得
-  number = document.getElementById("number");
-
-  // 数値を画面に表示
-  s = "移動距離";
-  setNumber();
-
-  // スワイプイベント設定
-  setSwipe("#swipe_area");
+  number = document.getElementById("number"); // 数値表示部分のDOM取得
+  s = "移動距離"; // 数値を画面に表示
+  setSwipe("#swipe_area"); // スワイプイベント設定
 });
