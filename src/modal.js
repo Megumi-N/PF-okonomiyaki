c79744781462694f54f7d482e9ed1bf208e7c8fb
noguchi <02125011a@gmail.com>
2021-09-29 07:09:59 +0900

    fix: 本番環境だけスワイプが動かないので調整

--- src/modal.js
+++ src/modal.js
@@ -14,6 +14,26 @@ const modalOpen = () => {
   modal.role = "dialog";
 };
 
+// modalの中にツイート機能を入れるメソッド
+const twitterText = () => {
+  const twitter = document.createElement("a");
+  twitter.href = `https://twitter.com/intent/tweet?&text=お好み焼きを返す技術力が今、試される...%0aスマホから遊んでね。%0a%23お好み焼き%0a%23くそアプリ%0a%23お好み焼きをひっくり返すだけのアプリ%0a&url=https://zen-sinoussi-c18c33.netlify.app/`;
+  twitter.id = "twitter";
+  twitter.setAttribute("target", "_blank");
+  twitter.setAttribute("rel", "noopener noreferrer");
+  twitter.innerText = "ツイートする";
+  modalBody.appendChild(twitter);
+};
+
+// モーダルの中身を編集するメソッド
+const modalModify = () => {
+  modalTitle.innerText = titleText;
+  modalBody.innerHTML = bodyText;
+  modalButton.innerText = "再挑戦";
+  modalButton.setAttribute("onclick", "window.location.reload();");
+  twitterText();
+};
+
 const pcBlockModal = () => {
   blockModal.classList.add("show");
   blockModal.style.display = "block";
